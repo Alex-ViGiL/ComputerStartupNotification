735424376dc8649e40f4ab55c9e4eb3a96f8919d
Myster-Sneppi <60942937+Alex-ViGiL@users.noreply.github.com>
2021-07-22 09:17:35 +0300

    CopyFile

---
+++ Curs/Curs/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Win32;
+using System.Reflection;
+
+namespace Curs
+{
+    public partial class Form1 : Form
+    {
+        string pathEmail = "Email.txt";
+        public Form1()
+        {
+            InitializeComponent();     
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            SetAutoRunValue(true, Assembly.GetExecutingAssembly().Location);
+            //Email
+            try
+            {
+                using (StreamReader sr = new StreamReader(pathEmail))
+                {
+                    textBox1.Text = sr.ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+            DateTime info = DateTime.Now;
+            Information information = new Information(info);
+            label5.Text = Convert.ToString(SystemInformation.UserName);
+            label6.Text = Convert.ToString(Environment.MachineName);
+
+            string writePath = "Info.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(writePath))
+                {
+                    string Text = "";
+                    foreach (char Symbol in sr.ReadToEnd())
+                    {
+                        if (Symbol != '&')
+                        {
+                            Text += Convert.ToString(Symbol);
+                        }
+                        else
+                        {
+                            listBox1.Items.Add(Text);
+                            Text = "";
+                        }
+                    }
+                }
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    MailAddress from = new MailAddress("botmessage0@gmail.com", "Bot");
+
+                    MailAddress to = new MailAddress(textBox1.Text);
+
+                    MailMessage m = new MailMessage(from, to);
+
+                    m.Subject = "Включение компютера";
+
+                    m.Body = $"<h2> Здравствуйте {SystemInformation.UserName} ваш пк был только что включён \n {info}</h2>";
+
+                    m.IsBodyHtml = true;
+
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+                    smtp.Credentials = new NetworkCredential("botmessage0@gmail.com", "smsbot12345");
+                    smtp.EnableSsl = true;
+                    smtp.Send(m);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Для решения данной проблемі нужно включить разрешение на сайте : https://myaccount.google.com/lesssecureapps");
+                throw;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(pathEmail, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(textBox1.Text);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private bool SetAutoRunValue(bool autorun, string path)
+        {
+            const string name = "Pc-On-Info";
+            string ExePath = path;
+            RegistryKey reg;
+
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                {
+                    reg.SetValue(name, ExePath);
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                }
+
+                reg.Flush();
+                reg.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Curs/Curs/Information.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Curs
+{
+    class Information
+    {
+       public Information(DateTime info) 
+        {
+            string writePath = "Info.txt";
+            string DateTime = Convert.ToString(info) + "&";
+
+            try
+            {
+                File.AppendAllText(writePath,DateTime, Encoding.UTF8);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+    }
+}
